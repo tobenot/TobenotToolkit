9ff7dd536fc87828ecd30c2b0032b90fc07d1184
tobenot <2818373670@qq.com>
2024-02-11 21:54:19 +0800

    插件框架提交

---
+++ Source/TobenotToolkit/Private/TobenotToolkitBPLibrary.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2024 tobenot
+// This code is licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include "TobenotToolkitBPLibrary.h"
+#include "TobenotToolkit.h"
+
+UTobenotToolkitBPLibrary::UTobenotToolkitBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
---
+++ Source/TobenotToolkit/Public/TobenotToolkitBPLibrary.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2024 tobenot
+// This code is licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "TobenotToolkitBPLibrary.generated.h"
+
+UCLASS()
+class UTobenotToolkitBPLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_UCLASS_BODY()
+	
+};
